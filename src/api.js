e0f15c1ac3d27b16122810ba6cf13a729c030fba
sandeeprawat28 <sandeeprwt58@gmail.com>
2022-10-25 21:22:23 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5a9d13375emshe84ecd14546b643p14aad1jsn8601cec62ad8",  // Please add your Weather api here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9dd5724e0485c9d408e06f25b52c003d";  // Please add your Weather api here
